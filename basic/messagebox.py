046f885a14a6dd32ef5c91692d5a30bc20ec7bbe
TaceyWong <xinyong.wang@qq.com>
2016-09-06 23:28:15 +0800

    消息框

---
+++ basic/messagebox.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+from PyQt4 import QtGui
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example,self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300,300,250,150)
+        self.setWindowTitle(u"Message-Box 消息框")
+        self.show()
+
+    def closeEvent(self,event):
+        reply = QtGui.QMessageBox.question(self,"Message",
+                u"你确定要退出?",
+                QtGui.QMessageBox.Yes|QtGui.QMessageBox.No,
+                QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+def main():
+    app =QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
